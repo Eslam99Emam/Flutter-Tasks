81c71111ec63ede8dc31c347762daca958e2a136
Eslam99Emam <eslam.emam.hussien@gmail.com>
2024-11-20 19:05:07 +0200

    Task 3 added

---
+++ Task3/Pages/BreakFast.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Breakfast_Page extends StatefulWidget {
+  const Breakfast_Page({super.key});
+
+  @override
+  State<Breakfast_Page> createState() => _Breakfast_PageState();
+}
+
+class _Breakfast_PageState extends State<Breakfast_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+        ),
+        title: Text(
+          "Breakfast",
+          style: Theme.of(context)
+              .textTheme
+              .displaySmall!
+              .apply(color: Colors.white),
+        ),
+        backgroundColor: Colors.pink[300],
+        elevation: 10,
+        shadowColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 6.0, crossAxisSpacing: 6.0),
+            itemCount: 12,
+            itemBuilder: (BuildContext context, i) {
+              return CustomCard("Breakfast ${i}", (i + 1) % 3 == 0);
+            }),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatefulWidget {
+  String name;
+
+  bool liked;
+  CustomCard(this.name, this.liked, {super.key});
+
+  @override
+  State<CustomCard> createState() => _CustomCardState();
+}
+
+class _CustomCardState extends State<CustomCard> {
+  GlobalKey key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.pink[200],
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(widget.name),
+          Image.network(
+            // width: ,
+            "https://www.specialitybreads.co.uk/wp-content/uploads/2013/12/iStock_000010433657Medium-1024x682.jpg",
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.liked = !widget.liked;
+              });
+            },
+            child: Icon(widget.liked ? Icons.favorite : Icons.favorite_border),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Task3/Pages/Dessert.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Dessert_Page extends StatefulWidget {
+  const Dessert_Page({super.key});
+
+  @override
+  State<Dessert_Page> createState() => _Dessert_PageState();
+}
+
+class _Dessert_PageState extends State<Dessert_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+        ),
+        title: Text(
+          "Desserts",
+          style: Theme.of(context)
+              .textTheme
+              .displaySmall!
+              .apply(color: Colors.white),
+        ),
+        backgroundColor: Colors.pink[300],
+        elevation: 10,
+        shadowColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 6.0, crossAxisSpacing: 6.0),
+            itemCount: 12,
+            itemBuilder: (BuildContext context, i) {
+              return CustomCard("Dessert ${i}", (i + 1) % 3 == 0);
+            }),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatefulWidget {
+  String name;
+
+  bool liked;
+  CustomCard(this.name, this.liked, {super.key});
+
+  @override
+  State<CustomCard> createState() => _CustomCardState();
+}
+
+class _CustomCardState extends State<CustomCard> {
+  GlobalKey key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.pink[200],
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(widget.name),
+          Image.network(
+            // width: ,
+            "https://www.specialitybreads.co.uk/wp-content/uploads/2013/12/iStock_000010433657Medium-1024x682.jpg",
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.liked = !widget.liked;
+              });
+            },
+            child: Icon(widget.liked ? Icons.favorite : Icons.favorite_border),
+          ),
+        ],
+      ),
+    );
+  }
+}
